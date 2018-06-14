73dcb7ec07d5b718cb526ad48a8b601d12203e93
Aaron Weatherall <aaron.weatherall@theiconic.com.au>
2018-06-14 13:33:37 +1000

    Fix ES6

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var { makeExecutableSchema } = require('graphql-tools');
-var { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+import express from 'express'
+import bodyParser from 'body-parser'
+import { makeExecutableSchema } from 'graphql-tools'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
   }
 `
+
 const resolvers = {
     Query: {
         hello: (_, { name }) => `Hello ${name || 'World'}`,
@@ -17,13 +18,13 @@ const resolvers = {
 export const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
-});
+})
 
-const PORT = 8080;
-const app = express();
+const PORT = 8080
+const app = express()
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }));
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }))
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
-app.listen(PORT);
-console.log(`Running a GraphQL API server at localhost:${ PORT }/graphql`);
+app.listen(PORT)
+console.log(`Running a GraphQL API server at localhost:${ PORT }/graphql`)
